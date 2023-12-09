e07926adad317416d2b53b981dd4d8a2978ca5d8
wzhzx <1005453581@qq.com>
2023-12-09 19:01:32 +0800

    init

---
+++ src/main/java/com/example/liteblog/LiteBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.liteblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/liteblog/LiteBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liteblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
